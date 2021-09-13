18263efb682ea0e5e3c855aa57b201e06a59f95a
Otee Ziregbe <otee.ziregbe@cyberspace.net.ng>
2021-09-13 07:06:28 +0100

    Add project files.

---
+++ MyContacts/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyContacts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+       //Commit 1
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok("ss");
+        }
+
+
+        [HttpGet("state/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            return Ok("ss");
+        }
+
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(string searchString)
+        {
+            return Ok("ss");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok("ss");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            return Ok("ss");
+        }
+
+    }
+}
